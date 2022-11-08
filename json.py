d3148c3748483ebe70c985ef9244e0bb76965301
Simeskaa <simen.hansen.skaara@gmail.com>
2022-11-08 17:04:36 +0100

    fungerende json decrypt

---
+++ json.py
@@ -0,0 +1,15 @@
+import jsonlib as json
+
+
+test = json.write({'sensor 1' : 25, 'sensor 2' : 13, 'sensor 3' : 1000},sort_keys=True, indent=' ').decode ('utf8')
+
+read = json.read(test)
+
+test_value = read['sensor 1']
+
+print(read['sensor 1'])
+print(read['sensor 2'])
+print(read['sensor 3'])
+print(test_value)
+
+
